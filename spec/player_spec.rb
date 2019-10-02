b995d7783b2c7e61d24796104f6d3e59ad7cfb74
AlecDWalker <walkeralec@hotmail.com>
2019-10-02 16:15:55 +0100

    skinny controllers

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,16 +7,11 @@ describe Player do
   it 'returns its name' do
     expect(player.name).to eq "Andy"
   end
-  
+
   describe '#receive_damage' do
     it "reduces attacked player's HP by 10" do
       player.receive_damage
       expect(player.hit_points).to eq 50
     end
   end
-
-  # describe '#hit_points' do
-  #   it ''
-  #   expect(player.hit_points).to eq 60
-  # end
 end
