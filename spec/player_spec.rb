b466f839fc2a1bf0ba213462910cc9ec13503b6f
Mezela <p_mezue@hotmail.com>
2019-10-02 15:48:57 +0100

    Single Responsibility Principle step complete

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,12 +4,10 @@ describe Player do
 
   subject(:player) {described_class.new("Andy")}
 
-  describe '#attack' do
-    it 'returns its name' do
-      expect(player.name).to eq "Andy"
-    end
+  it 'returns its name' do
+    expect(player.name).to eq "Andy"
   end
-
+  
   describe '#receive_damage' do
     it "reduces attacked player's HP by 10" do
       player.receive_damage
